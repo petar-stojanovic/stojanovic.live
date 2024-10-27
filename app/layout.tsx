7ed78eeb8b0263a5fccef47c358ff6815e7673e7
petar-stojanovic <banakafala1a@gmail.com>
2024-10-27 16:53:19 +0100

    biome formatting fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,26 @@
-import type { Metadata } from "next";
+import {Rubik} from "next/font/google";
 import "./globals.css";
-import { Rubik } from "next/font/google";
+import type {Metadata} from "next";
 
 const rubik = Rubik({
-  subsets: ["latin"],
-  style: ["normal", "italic"],
-  display: "swap",
+    subsets: ["latin"],
+    style: ["normal", "italic"],
+    display: "swap",
 });
+
 export const metadata: Metadata = {
-  title: "Petar Stojanovic - Portfolio",
-  description: "Petar Stojanovic's portfolio website.",
+    title: "Petar Stojanovic - Portfolio",
+    description: "Petar Stojanovic's portfolio website.",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className={rubik.className}>
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en" className={rubik.className}>
+        <body>{children}</body>
+        </html>
+    );
 }
