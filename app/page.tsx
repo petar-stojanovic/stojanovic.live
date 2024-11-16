887bbf55bf50938ed6b4eb032a4e54c28070428c
petar-stojanovic <banakafala1a@gmail.com>
2024-11-16 15:36:25 +0100

    added profile picture

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="">
-      <div className={"h-[100px] w-[100px] bg-red-600"} />
+      <div className="flex items-center gap-10 py-4">
+        <Image
+          src={"/profile.jpg"}
+          width={100}
+          height={100}
+          alt="Profile Picture"
+          className="h-32 w-32 rounded-full border"
+        />
+        <div className="flex flex-col gap-1">
+          <h1 className="font-bold text-3xl">Petar Stojanović</h1>
+          <p className="text-xl">Software Engineer</p>
+        </div>
+      </div>
     </div>
   );
 }
