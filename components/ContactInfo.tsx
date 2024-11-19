1d2ea04a14143af1d2bf0282a075acf4461adf08
petar-stojanovic <banakafala1a@gmail.com>
2024-11-19 18:30:16 +0100

    refactored code. added labels on hover for skills

--- components/ContactInfo.tsx
+++ components/ContactInfo.tsx
@@ -3,16 +3,16 @@ import ThemeIcon from "@/app/_components/ThemeIcon";
 const ContactInfo = () => {
   return (
     <section className="flex flex-wrap gap-2 sm:gap-3">
-      <button
-        type="button"
-        className="rounded-xl border border-neutral-200 bg-zinc-100 p-3 hover:border-neutral-300 hover:bg-zinc-200 dark:border-neutral-900 dark:bg-zinc-900 dark:hover:border-neutral-800 dark:hover:bg-zinc-800 "
-      >
-        <a
-          href="https://www.github.com/petar-stojanovic"
-          target="_blank"
-          rel="noreferrer"
+      <div className="group relative">
+        <button
+          type="button"
+          className="rounded-xl border border-neutral-200 bg-zinc-100 p-3 hover:border-neutral-300 hover:bg-zinc-200 dark:border-neutral-900 dark:bg-zinc-900 dark:hover:border-neutral-800 dark:hover:bg-zinc-800 "
         >
-          <div className="flex items-center justify-center">
+          <a
+            href="https://www.github.com/petar-stojanovic"
+            target="_blank"
+            rel="noreferrer"
+          >
             <ThemeIcon
               iconName="github"
               alt="Github"
@@ -20,20 +20,23 @@ const ContactInfo = () => {
               height={24}
               className="h-8 w-8"
             />
-          </div>
-        </a>
-      </button>
+          </a>
+        </button>
+        <div className="pointer-events-none absolute right-0 left-0 z-10 mt-1 w-fit justify-center rounded-xl border border-neutral-800 bg-zinc-950 px-4 py-1 opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">
+          Github
+        </div>
+      </div>
 
-      <button
-        type="button"
-        className="rounded-xl border border-neutral-200 bg-zinc-100 p-3 hover:border-neutral-300 hover:bg-zinc-200 dark:border-neutral-900 dark:bg-zinc-900 dark:hover:border-neutral-800 dark:hover:bg-zinc-800 "
-      >
-        <a
-          href="https://www.linkedin.com/in/petar-stojanovikj"
-          target="_blank"
-          rel="noreferrer"
+      <div className="group relative">
+        <button
+          type="button"
+          className="rounded-xl border border-neutral-200 bg-zinc-100 p-3 hover:border-neutral-300 hover:bg-zinc-200 dark:border-neutral-900 dark:bg-zinc-900 dark:hover:border-neutral-800 dark:hover:bg-zinc-800 "
         >
-          <div className="flex items-center justify-center">
+          <a
+            href="https://www.linkedin.com/in/petar-stojanovikj"
+            target="_blank"
+            rel="noreferrer"
+          >
             <ThemeIcon
               iconName="linkedin"
               alt="Linkedin"
@@ -41,20 +44,23 @@ const ContactInfo = () => {
               height={24}
               className="h-8 w-8"
             />
-          </div>
-        </a>
-      </button>
+          </a>
+        </button>
+        <div className="pointer-events-none absolute right-0 left-0 z-10 mt-1 w-fit justify-center rounded-xl border border-neutral-800 bg-zinc-950 px-4 py-1 opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">
+          Linkedin
+        </div>
+      </div>
 
-      <button
-        type="button"
-        className="rounded-xl border border-neutral-200 bg-zinc-100 p-3 hover:border-neutral-300 hover:bg-zinc-200 dark:border-neutral-900 dark:bg-zinc-900 dark:hover:border-neutral-800 dark:hover:bg-zinc-800 "
-      >
-        <a
-          href="mailto:2001petarstojanovic@gmail.com"
-          target="_blank"
-          rel="noreferrer"
+      <div className="group relative">
+        <button
+          type="button"
+          className="rounded-xl border border-neutral-200 bg-zinc-100 p-3 hover:border-neutral-300 hover:bg-zinc-200 dark:border-neutral-900 dark:bg-zinc-900 dark:hover:border-neutral-800 dark:hover:bg-zinc-800 "
         >
-          <div className="flex items-center justify-center ">
+          <a
+            href="mailto:2001petarstojanovic@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
             <ThemeIcon
               iconName="email"
               alt="Email"
@@ -62,25 +68,30 @@ const ContactInfo = () => {
               height={24}
               className="h-8 w-8"
             />
-          </div>
-        </a>
-      </button>
+          </a>
+        </button>
+        <div className="pointer-events-none absolute right-0 left-0 z-10 mt-1 w-fit justify-center rounded-xl border border-neutral-800 bg-zinc-950 px-4 py-1 opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">
+          Email
+        </div>
+      </div>
 
-      <button
-        type="button"
-        className="rounded-xl border border-neutral-200 bg-zinc-100 p-3 hover:border-neutral-300 hover:bg-zinc-200 dark:border-neutral-900 dark:bg-zinc-900 dark:hover:border-neutral-800 dark:hover:bg-zinc-800 "
-      >
-        <a
-          href="https://drive.google.com/file/d/1RSrJhWM5Giz8p8FcGhjCJkm82UMS_-vJ/view"
-          target="_blank"
-          rel="noreferrer"
+      <div className="group relative">
+        <button
+          type="button"
+          className="rounded-xl border border-neutral-200 bg-zinc-100 p-3 hover:border-neutral-300 hover:bg-zinc-200 dark:border-neutral-900 dark:bg-zinc-900 dark:hover:border-neutral-800 dark:hover:bg-zinc-800 "
         >
-          <div className="flex items-center justify-center gap-1">
-            <ThemeIcon iconName="resume" alt="Resume" width={30} height={30} />
-            <span>Resume</span>
-          </div>
-        </a>
-      </button>
+          <a
+            href="https://drive.google.com/file/d/1RSrJhWM5Giz8p8FcGhjCJkm82UMS_-vJ/view"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <ThemeIcon iconName="resume" alt="Resume" width={32} height={32} />
+          </a>
+        </button>
+        <div className="pointer-events-none absolute right-0 left-0 z-10 mt-1 w-fit justify-center rounded-xl border border-neutral-800 bg-zinc-950 px-4 py-1 opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">
+          Resume
+        </div>
+      </div>
     </section>
   );
 };
