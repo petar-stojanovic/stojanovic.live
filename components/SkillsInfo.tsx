7048047cae1916590ba185c5be4ed6679fa4f69f
petar-stojanovic <banakafala1a@gmail.com>
2024-11-19 20:07:47 +0100

    added icons to public folder

--- components/SkillsInfo.tsx
+++ components/SkillsInfo.tsx
@@ -9,13 +9,15 @@ const SkillsInfo = () => {
 
       <div className="group relative w-fit">
         <div className="rounded-xl border border-neutral-300 bg-zinc-100 p-1 duration-200 ease-in-out hover:border-neutral-400 hover:bg-zinc-200 dark:border-neutral-800 dark:bg-zinc-900 dark:hover:bg-zinc-800">
-          <Image
-            src="/skills/angular.svg"
-            width={48}
-            height={48}
-            alt="Angular"
-            className="m-2"
-          />
+          <div className="mx-2 my-1 flex flex-col items-center justify-center gap-1">
+            <Image
+              src="/skills/angular.svg"
+              width={48}
+              height={48}
+              alt="Angular"
+            />
+            <span>Angular</span>
+          </div>
         </div>
         <div className="pointer-events-none absolute right-0 left-0 z-10 mt-1 w-fit justify-center rounded-xl border border-neutral-800 bg-zinc-950 px-4 py-1 text-background opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100 dark:text-foreground">
           Angular
